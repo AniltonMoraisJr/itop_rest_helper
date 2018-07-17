3b726f866b25b197fd03a4060efd5e7e2a3c41fb
Anilton Francisco de Morais Jr <anilton.francisco@gmail.com>
2018-07-17 12:42:56 -0300

    Correcting the .env parameters in the config file

--- src/ItopRestHelper.php
+++ src/ItopRestHelper.php
@@ -5,11 +5,11 @@ namespace AniltonMoraisJr\ItopRestHelper;
 class ItopRestHelper {
 	
 	public function consultaAPIItop($object){
-		$url = config('itopresthelper.URL_API_NETADMIN_REST');
+		$url = config('itopresthelper.URL_API_REST');
 
         $objectJSON = json_encode($object);
         $client = new \GuzzleHttp\Client();
-        $response = $client->post($url, ['body'=>['auth_user' => config('itopresthelper.USER_API_NETADMIN'), 'auth_pwd' => config('itopresthelper.PASS_API_NETADMIN'), 'json_data' => $objectJSON]]);
+        $response = $client->post($url, ['body'=>['auth_user' => config('itopresthelper.USER_API'), 'auth_pwd' => config('itopresthelper.PASS_API'), 'json_data' => $objectJSON]]);
      
         $obj = $response->json();
 
--- src/config/itopresthelper.php
+++ src/config/itopresthelper.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'URL_API_NETADMIN_REST' => env('URL_API_NETADMIN', 'http://servicedesk.com.br/webservices/rest.php?version=1.0'),
-    'URL_API_NETADMIN_FULL' => env('URL_API_NETADMIN_FULL', 'http://servicedesk.com.br'),
-    'USER_API_NETADMIN' => env('USER_API_NETADMIN', 'admin'),
-    'PASS_API_NETADMIN' => env('PASS_API_NETADMIN', 'admin'),
+    'URL_API_REST' => env('URL_API_REST', 'http://servicedesk.com.br/webservices/rest.php?version=1.0'),
+    'URL_API_FULL' => env('URL_API_FULL', 'http://servicedesk.com.br'),
+    'USER_API' => env('USER_API', 'admin'),
+    'PASS_API' => env('PASS_API', 'admin'),
 ];
